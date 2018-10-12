d4261a70b36d51eb35b9d474238e1dd4ec5a8575
Van Lao <laominhvan1994@gmail.com>
2018-10-11 17:03:46 -0700

    Initial Commit

---
+++ WareHouse_Inventory_demo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WareHouse_Inventory_demo
+//
+//  Created by Tu Pham on 10/11/18.
+//  Copyright © 2018 Van Lao. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
